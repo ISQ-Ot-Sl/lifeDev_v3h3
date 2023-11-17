c7454856de9c17efe9206e4ab9c3900cc01fe6b5
ISQ-Ot-Sl <isaque.otero1h3@gmail.com>
2023-11-16 22:42:13 -0300

    Aula1611 Conectores do firebase

---
+++ lifedev/src/hooks/userAuthentication.jsx
@@ -0,0 +1,43 @@
+import { db} from "../firebase/config";
+import {
+    getAuth,
+    createUserWithEmailAndPasswor,
+    signInWithEmailAndPassword,
+    updateProfile,
+    signOut,
+}from 'firebase/auth'
+
+import { useState, useEffect } from "react"
+
+export const userAuthentication = () =>{
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState (null)
+    const [cancelled, setCancelled] = useState (false)
+
+    const auth = getAuth()
+
+    function checkIfCancelled(){
+        if(cancelled){
+            return
+        }
+    }
+
+    async function createUser (data){
+        checkIfCancelled()
+
+        setLoading(true)
+        setError(null)
+
+        try{
+            const { user } = await createUserWithEmailAndPasswor(
+                auth,
+                date.email,
+                data.password
+    
+            )
+        }catch(error) {
+            
+        }
+        
+    }
+}
